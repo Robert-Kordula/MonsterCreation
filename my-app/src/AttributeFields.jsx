d71a1744165e0833236608f5a24da5f15dc3da48
Robert Kordula <bobbykordula@gmail.com>
2021-12-12 17:23:07 +0000

    Reworked attributesList in AttributeFields, split into groups 'About', 'Stats', 'Saves', 'Skills' About is for general information about the monster, things like name, group and eventually an image to represent the monster Stats is for the monsters stats that governs any skill checks Saves is for the monsters stats that govern their resistances to damages or for their savings throws Skills is for lists of actions, skills, etc
    All attributes are currently coded with how what data they will take in
    For attributes that take in a table added functionality to convert the data into a viewable table

--- my-app/src/AttributeFields.jsx
+++ my-app/src/AttributeFields.jsx
@@ -1,12 +1,272 @@
 import './App.css';
-import { TextField, Fab, Box } from '@mui/material';
+import { TextField, Fab, Box, List, TableContainer, Table, TableHead, TableBody, TableRow, TableCell } from '@mui/material';
 import { useParams } from 'react-router';
 
 let monsterData;
 
 function AttributeFields(props) {
 
-    const attributeList = ['name', 'img_main', 'size', 'type', 'subtype', 'group', 'alignment', 'armor_class', 'armor_desc', 'hit_points', 'hit_dice', 'speed', 'strength', 'dexterity', 'constitution', 'intelligence', 'wisdom', 'charisma', 'strength_save', 'dexterity_save', 'constitution_save', 'intelligence_save', 'wisdom_save', 'charisma_save', 'perception', 'skills', 'damage_vulnerabilities', 'damage_resistances', 'damage_immunities', 'condition_immunities', 'senses', 'languages', 'challenge_rating', 'actions', 'reactions', 'legendary_desc', 'special_abilities', 'spell_list', 'document__slug', 'document__title', 'document__license_url'];
+    const aboutAttributes = [
+        {
+            'name': {
+                value: 'string'
+            }
+        },
+        {
+            'img_main': {
+                value: 'image'
+            }
+        },
+        {
+            'size': {
+                value: 'string'
+            }
+        },
+        {
+            'type': {
+                value: 'string'
+            }
+        },
+        {
+            'subtype': {
+                value: 'string'
+            }
+        },
+        {
+            'group': {
+                value: 'string'
+            }
+        },
+        {
+            'alignment': {
+                value: 'string'
+            }
+        },
+        {
+            'languages': {
+                value: 'string'
+            }
+        },
+        {
+            'challenge_rating': {
+                value: 'integer'
+            }
+        },
+        {
+            'document__slug': {
+                value: 'string'
+            }
+        },
+        {
+            'document__title': {
+                value: 'string'
+            }
+        },
+        {
+            'document__license_url': {
+                value: 'string'
+            }
+        }
+    ];
+    const statsAttributes = [
+        {
+            'armor_class': {
+                value: 'integer'
+            }
+        },
+        {
+            'armor_desc': {
+                value: 'string'
+            }
+        },
+        {
+            'hit_points': {
+                value: 'integer'
+            }
+        },
+        {
+            'hit_dice': {
+                value: 'string'
+            }
+        },
+        {
+            'speed': {
+                value: 'string'
+            }
+        },
+        {
+            'strength': {
+                value: 'integer'
+            }
+        },
+        {
+            'dexterity': {
+                value: 'integer'
+            }
+        },
+        {
+            'constitution': {
+                value: 'integer'
+            }
+        },
+        {
+            'intelligence': {
+                value: 'integer'
+            }
+        },
+        {
+            'wisdom': {
+                value: 'integer'
+            }
+        },
+        {
+            'charisma': {
+                value: 'integer'
+            }
+        },
+        {
+            'perception': {
+                value: 'integer'
+            }
+        },
+        {
+            'senses': {
+                value: 'integer'
+            }
+        }
+    ];
+    const savesAttributes = [
+        {
+            'strength_save': {
+                value: 'integer'
+            }
+        },
+        {
+            'dexterity_save': {
+                value: 'integer'
+            }
+        },
+        {
+            'constitution_save': {
+                value: 'integer'
+            }
+        },
+        {
+            'intelligence_save': {
+                value: 'integer'
+            }
+        },
+        {
+            'wisdom_save': {
+                value: 'integer'
+            }
+        },
+        {
+            'charisma_save': {
+                value: 'integer'
+            }
+        },
+        {
+            'damage_vulnerabilities': {
+                value: 'string'
+            }
+        },
+        {
+            'damage_resistances': {
+                value: 'string'
+            }
+        },
+        {
+            'damage_immunities': {
+                value: 'string'
+            }
+        },
+        {
+            'condition_immunities': {
+                value: 'string'
+            }
+        }
+    ];
+    const skillsAttributes = [
+        {
+            'skills': {
+                value: 'list',
+            }
+        },
+        {
+            'actions': {
+                value: 'table',
+                table: [
+                    {
+                        header: 'name',
+                        value: 'string'
+                    },
+                    {
+                        header: 'desc',
+                        value: 'string'
+                    },
+                    {
+                        header: 'attack_bonus',
+                        value: 'integer'
+                    },
+                    {
+                        header: 'damage_dice',
+                        value: 'string'
+                    }, 
+                    {
+                        header: 'damage_bonus',
+                        value: 'integer'
+                    }
+                ]
+            }
+        },
+        {
+            'reactions': {
+                value: 'list'
+            }
+        },
+        {
+            'legendary_desc': {
+                value: 'list'
+            }
+        },
+        {
+            'legendary_actions': {
+                value: 'table',
+                table: [
+                    {
+                        header: 'name',
+                        value: 'string'
+                    },
+                    {
+                        header: 'desc',
+                        value: 'string'
+                    }
+                ]
+            }
+        },
+        {
+            'special_abilities': {
+                value: 'table',
+                table: [
+                    {
+                        header: 'name',
+                        value: 'string'
+                    },
+                    {
+                        header: 'desc',
+                        value: 'string'
+                    }
+                ]
+            }
+        },
+        {
+            'spell_list': {
+                value: 'list'
+            }
+        }
+    ];
+    const attributeList = [];
     const { slug } = useParams();
     console.log(slug);
     monsterData = props.monster || {};
@@ -25,53 +285,170 @@ function AttributeFields(props) {
         let url = 'http://localhost:3500/monster';
         if (props.method === 'PATCH') {
             url = url.concat(`/${slug}`);
-        } 
-        
+        }
+
         let response = await fetch(url, requestOptions);
-        alert(response.status);    
+        alert(response.status);
+    };
+
+    const buildField = (attribute) => {
+        let label = Object.keys(attribute)[0];
+        return <Attribute key={label} name={label} attribute={attribute} />;
     };
 
     return (
         <div>
             <header id='header-title'>{props.name || ''}</header>
             <TextField
-                    id='slug'
-                    defaultValue={slug}
-                    label='slug'
-                    type='search'
-                    disabled
-                />
-            <Box
-                component="form"
-                sx={{
-                    '& .MuiTextField-root': { m: 1, width: '25ch' },
-                }}
-                noValidate
-                autoComplete="off"
-            >
-                {attributeList.map((attribute) => (
-                    <Attribute key={attribute} name={attribute} />
-                ))}
+                id='slug'
+                defaultValue={slug}
+                label='slug'
+                type='search'
+                disabled
+            />
+            <Box>
+                <Box
+                    component="form"
+                    sx={{
+                        '& .MuiTextField-root': { m: 1, width: '25ch' },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                >
+                    {aboutAttributes.map(buildField)}
+                </Box>
+                <Box
+                    component="form"
+                    sx={{
+                        '& .MuiTextField-root': { m: 1, width: '25ch' },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                >
+                    {statsAttributes.map(buildField)}
+                </Box>
+                <Box
+                    component="form"
+                    sx={{
+                        '& .MuiTextField-root': { m: 1, width: '25ch' },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                >
+                    {savesAttributes.map(buildField)}
+                </Box>
+                <Box
+                    component="form"
+                    sx={{
+                        '& .MuiTextField-root': { m: 1, width: '25ch' },
+                    }}
+                    noValidate
+                    autoComplete="off"
+                >
+                    {skillsAttributes.map(buildField)}
+                </Box>
             </Box>
-            <Fab 
-                className='Buttons UpdateButton' 
-                variant='extended' 
-                size='medium' 
+            <Fab
+                className='Buttons UpdateButton'
+                variant='extended'
+                size='medium'
                 id='UpdateButton'
                 onClick={sendMonster}>
-                    SAVE CHANGES</Fab>
+                SAVE CHANGES</Fab>
         </div>
     )
 }
 
 function Attribute(params) {
+    let attribute = params.attribute;
+    let label = Object.keys(attribute)[0];
+    let value = attribute[label]['value'];
+    console.log(`${attribute}\n${label}\n${value}`);
+    if (value === 'list') {
+        return (
+            <List
+                id={params.name}
+                subheader={params.name}
+                variant="filled"
+            />
+        );
+    } else if (value === 'table') {
+        let table = attribute[label]['table'];
+        console.log(table);
+        return (
+            <GenerateTable
+                id={label}
+                table={table}
+            />
+        )
+    }
+    else {
+        return (
+            <TextField
+                id={label}
+                label={label}
+                defaultValue={monsterData[label] || 'N/A'}
+                variant="filled"
+            />
+        );
+    }
+}
+
+function GenerateTable(params) {
+    let table = params.table;
+    let label = params.id;
     return (
-        <TextField
-            id={params.name}
-            label={params.name}
-            defaultValue= {monsterData[params.name] || 'N/A'}
-            variant="filled"
-        />
+        <TableContainer>
+            <p>{params.id}</p>
+            <Table>
+                <TableHead>
+                    <GenerateHeaders
+                        table={table}
+                        label = {label}
+                    />
+                </TableHead>
+                <GenerateBody
+                    headers = {table}
+                    data = {monsterData[label]}
+                />
+            </Table>
+        </TableContainer>
+    )
+}
+
+function GenerateHeaders(params) {
+    let headers = params.table;
+    let id = params.label;
+    return (
+        <TableRow>
+            {headers.map((column) => (
+                <TableCell
+                    key={`${id}-${column.header}`}
+                >
+                    {column.header}
+                </TableCell>
+            ))}
+        </TableRow>
+    )
+}
+
+function GenerateBody(params) {
+    let headers = params.headers;
+    let data = params.data;
+    console.log(data.length);
+    return (
+        <TableBody>
+            {data.map((row) => (
+                <TableRow>
+                    {headers.map((column) => (
+                        <TableCell
+                        >
+                            {row[column.header]}
+                        </TableCell>
+                    ))}    
+                </TableRow>
+            ))}
+        </TableBody>
     )
 }
 
